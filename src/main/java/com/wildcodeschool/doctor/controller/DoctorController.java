b1c693d2ad1f891b00f36b5f48fd0b0879e753b5
julien <julienmulle@gmail.com>
2022-07-25 16:25:19 +0200

    add controller

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,25 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Doctor doctor(@PathVariable int number) {
+        if(number <13 ){
+            throw  new ResponseStatusException(HttpStatus.SEE_OTHER,"oups, i did it again");
+        }
+        if(number>14){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Et non... ");
+        }
+        return new Doctor(13,"Jodie Whitteker");
     }
 }
